052d05f9d24b4e02cd64dfe69b37b06274dce3ec
Nicolás Fagetti <nicolas.moro.fagetti@gmail.com>
2025-06-26 01:50:26 -0300

    Moved old code to specific folder

--- sockets_ftp/server/refactoring/config.h
+++ sockets_ftp/server/refactoring/config.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#define APP_NAME "miniftp"
+#define APP_NAME "myftp"
 #define VERSION "1.0"
 #define OS_NAME "Linux"
 #define BANNER APP_NAME" version "VERSION" Ready"
@@ -12,9 +12,10 @@
 #define FTP_PORT PORT_DEFAULT
 #define LOCALHOST "127.0.0.1"
 #define BUFSIZE 512
+#define USERNAME_MAX 64
 
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
 
-#define PORT_DOC "Port number (default: " STR(PORT_DEFAULT) ")"
+#define PORT_DOC "Port number (default: " STR(FTP_PORT) ")"
 #define ADDR_DOC "Local IP address (default: " LOCALHOST ")"
